da36cc50d4411743f6fd7a14d5acfc99afc85298
Rebecca Stone <ysbecca@gmail.com>
2018-05-16 12:29:32 +0100

    got rid of bootstrap dependency

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 # from flask_migrate import Migrate
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 # Add the Bootstrap markup
-Bootstrap(app)
+# Bootstrap(app)
 
 # Create an instance of the DB
 # db = SQLAlchemy(app)
