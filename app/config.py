cc0ece41d3bff347593ed93c132dccc724f6ae5a
Rebecca Stone <ysbecca@gmail.com>
2018-06-03 22:54:53 +0100

    simplified to remove all db

--- app/config.py
+++ app/config.py
@@ -1,15 +1,49 @@
+import csv
 import os
+from os import listdir
+from os.path import isfile, join
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 # Annotation saving directories
 ANNOTATION_DIR = "/Users/ysbecca/ysbecca-projects/fwat/temp/"
 ANNOTATION_TYPES = ["EP", "SM"]
 
+
+DATASET = "BCSP Expert Training Set"
+STUDY = "EP/SM Annotation BCSP Expert"
+
+# Case info (equivalent to fill_db.py)
+case_dir = "/Users/ysbecca/ysbecca-projects/iciar-2018/data/WSI_xml/"
+csv_path = "/Users/ysbecca/ysbecca-projects/fwat/test_cases.csv"
+
+# Read case numbers from CSV file.
+IMAGE_PATHS = []
+IMAGES = []
+cases = []
+
+with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+        for row in reader:
+                cases.append(row[0])
+
+print("Found", len(cases), "cases.")
+
+for case in cases:
+        this_case_dir = case_dir + "Case_" + case
+        files = [file for file in listdir(this_case_dir) if isfile(join(this_case_dir, file))]
+        for f in files:
+                if '.svs' in f:
+                        IMAGE_PATHS.append(this_case_dir + "/")
+                        IMAGES.append(f)
+
+IMAGE_COUNT = len(IMAGE_PATHS)
+
+print("Found", IMAGE_COUNT, "images from all cases.")
+
+# DeepZoom config parameters
 
 DEEPZOOM_SLIDE=None		# View one slide at a time
 DEEPZOOM_FORMAT='jpeg'
--- app/views.py
+++ app/views.py
@@ -11,53 +11,12 @@ from io import BytesIO
 import json
 import ast
 
-@app.route('/')
-def index():
-    user = {'name': 'Rebecca Stone'}
 
-    # Show all the datasets and studies on the dashboard
-    datasets = Dataset.query.all()
-    studies = Study.query.all()
-
-    return render_template('index.html',
-    					   datasets=datasets,
-    					   studies=studies,
-                           user=user)
-
-@app.route('/new_dataset', methods=['GET', 'POST'])
-def new_dataset():
-	form = DatasetForm(request.form)
-	if request.method == 'POST' and form.validate():
-		print(form.name.data)
-		print(form.directory.data)
-		print(form.images.data)
-
-		# dataset = Dataset(form.name.data, form.images.data)
-		# db_session.add(dataset)
-
-		flash('New dataset created:' + str(form.name.data))
-		return redirect(url_for('/new_dataset'))
-
-	return render_template('new_dataset.html', form=form)
-
-# @app.route('/view/<int:study_id>/<int:image_id>')
-
-@app.route('/run_study/<int:study_id>')
-def run_study(study_id):
-	study = Study.query.get(study_id)
-	dataset = study.dataset
-	image_num = 0
-	# return redirect(url_for('/view_single', 
-						# study_id=study.id,
-						# dataset_id=dataset.id, 
-						# image_num=0))
-	return view_single(study.id, dataset.id, 0)
-
-@app.route('/view_study/<int:study_id>')
-def view_study(study_id):
-	study = Study.query.get(study_id)
-	dataset = study.dataset
-	image_count = dataset.images.count()
+@app.route('/view_study/')
+def view_study():
+	study = app.config["STUDY"]
+	dataset = app.config["DATASET"]
+	image_count = app.config["IMAGE_COUNT"]
 
 	return render_template('view_study.html',
 							study=study,
@@ -65,31 +24,23 @@ def view_study(study_id):
 							image_count=image_count)
 
 # Temporary single page for viewing a single WSI - testing only.
-@app.route('/view_single/<int:study_id>/<int:image_num>')
-def view_single(study_id, image_num):
+@app.route('/view_single/<int:image_num>')
+def view_single(image_num):
 
+	study = app.config["STUDY"]
+	dataset = app.config["DATASET"]
+	image_count = app.config["IMAGE_COUNT"]
 
-	# Test case only
-	if study_id == -1:
-		image_id = 102
-		image_dir = "/Users/ysbecca/ysbecca-projects/iciar-2018/data/WSI_xml/Case_0001/"
-		file_name = "A01.svs"
+	if image_num >= image_count or image_num < 0:
+		print("At end or beginning of study.")
+		# Redirect to study page - study completed, or back to beginning.
+		return render_template('view_study.html',
+					study=study,
+					dataset=dataset,
+					image_count=image_count)
 	else:
-		study = Study.query.get(study_id)
-		dataset = study.dataset
-		image_count = dataset.images.count()
-		if image_num >= image_count or image_num < 0:
-			print("At end or beginning of study.")
-			# Redirect to study page - study completed, or back to beginning.
-			return render_template('view_study.html',
-						study=study,
-						dataset=dataset,
-						image_count=image_count)
-		else:
-			image = dataset.images[image_num]
-			image_id = image.id
-			image_dir = image.file_dir
-			file_name = image.file_name
+		image_dir = app.config["IMAGE_PATHS"][image_num]
+		file_name = app.config["IMAGES"][image_num]
 
 	# Set single WSI options
 	config_map = {
@@ -122,28 +73,16 @@ def view_single(study_id, image_num):
 			slide_slug: DeepZoomGenerator(slide, **opts)
 		}
 		app.config['DEEPZOOM_SLIDE'] = image_dir + file_name
-
-		app.associated_images = []
 		app.slide_properties = slide.properties
-		for name, image in slide.associated_images.items():
-			app.associated_images.append(name)
-			slug = slugify(name)
-			app.slides[slug] = DeepZoomGenerator(ImageSlide(image), **opts)
-
-
 		slide_url = url_for('dzi', slug=slide_slug)
-		associated_urls = dict((file_name, url_for('dzi', slug=slugify(file_name)))
-		        for file_name in app.associated_images)
 
 	return render_template('view_single.html', slide_url=slide_url,
-            associated=associated_urls, #, properties=app.slide_properties,
             slide_mpp=app.slide_mpp,
-            image_id=image_id,
             image_dir=image_dir,
             file_name=file_name,
             x=x, y=y,
             study=study,
-            dataset_id=dataset.id,
+            dataset=dataset,
             image_num=image_num,
             image_count=image_count,
             annotation_types=app.config['ANNOTATION_TYPES'])
@@ -158,7 +97,7 @@ def save_annotations():
     key, value = data.popitem()
     parsed_data = ast.literal_eval(key)
     paths = parsed_data["paths"]
-    image_id = parsed_data["image_id"]
+
     wsi_x, wsi_y = parsed_data["wsi_x"], parsed_data["wsi_y"]
     code = parsed_data["code"]
     file_name = parsed_data["file_name"]
@@ -179,7 +118,6 @@ def dzi(slug):
     except KeyError:
         # Unknown slug
         abort(404)
-
 
 @app.route('/<slug>_files/<int:level>/<int:col>_<int:row>.<format>')
 def tile(slug, level, col, row, format):
